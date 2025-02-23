3b95ffcc293e872fe61e513fc209b330bce5d82c
Mateusz Urbaniak <urban1998@wp.pl>
2025-02-23 08:27:31 +0100

    Add example of creating multiple threads

---
+++ Thread/intro.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+
+void print(int n, const std::string &str)  {
+  std::string msg = std::to_string(n) + " : " + str;
+  std::cout << msg  << std::endl;
+}
+ 
+int main() {
+  std::vector<std::string> s = {
+      "Educative.blog",
+      "Educative",
+      "courses",
+      "are great"
+  };
+  std::vector<std::thread> threads;
+ 
+  for (int i = 0; i < s.size(); i++) {
+    threads.push_back(std::thread(print, i, s[i]));
+  }
+ 
+  for (auto &th : threads) {
+    th.join();
+  }
+  return 0;
+}
