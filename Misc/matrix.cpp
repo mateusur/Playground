88cc2032f72f4a94f42ab98718a2d6805bd9d379
Mateusz Urbaniak <urban1998@wp.pl>
2025-03-09 09:40:27 +0100

    Add another approach to matrix

--- Misc/matrix.cpp
+++ Misc/matrix.cpp
@@ -27,7 +27,37 @@ void matrix_contiguous() {
   std::cout << '\n';
   delete array;
 }
+
+void matrix() {
+  constexpr int col = 3;
+  constexpr int rows = 5;
+
+  int** array = new int*[rows];
+  for (int i = 0; i < rows; ++i) {
+    array[i] = new int[col];
+  }
+
+  for (int r = 0; r < rows; ++r) {
+    for (int c = 0; c < col; ++c) {
+      array[r][c] = r * col + c + 1;
+    }
+  }
+
+  for (int r = 0; r < rows; ++r) {
+    for (int c = 0; c < col; ++c) {
+      std::cout << std::setw(2) << array[r][c] << ' ';
+    }
+    std::cout << std::endl;
+  }
+
+  for (int r = 0; r < rows; ++r) {
+    delete[] array[r];
+  }
+  delete[] array;
+}
+
 int main() {
   matrix_contiguous();
+  matrix();
   return 0;
 }
